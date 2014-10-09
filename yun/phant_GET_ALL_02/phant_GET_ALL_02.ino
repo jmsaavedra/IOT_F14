f9e0e9ed12b68bfb82dbb9d55864ba978d7a6882
jmsaavedra <mailbox.jms@gmail.com>
2014-10-09 06:01:33 -0400

    added new phant examples

--- yun/phant_GET_ALL_02/phant_GET_ALL_02.ino
+++ yun/phant_GET_ALL_02/phant_GET_ALL_02.ino
@@ -1,7 +1,8 @@
 /*
-Simple data.sparkfun.com example
+Simple phant.io example
 
-go there to get the public and private keys needed to make this example work
+GET data from server. Build the URL using the SERVERADDRESS+PUBLICKEY to request
+.csv, .json, .jsonp, or .atom data returned
 
  */
 
@@ -9,15 +10,9 @@ go there to get the public and private keys needed to make this example work
 // include all Libraries needed:
 #include <Bridge.h>
 #include <HttpClient.h>
-#include "passwords.h"      // contains my passwords, see below
 
-/* NOTE: passwords.h is not included with this repo because it contains my passwords.
- You need to create it for your own version of this application.  To do so, make
- a new tab in Arduino, call it passwords.h, and include the following variables and constants:
-
- #define PUBLICKEY        "foo"                 // replace your phant public key here
- #define PRIVATEKEY       "0000"                // replace your phant secret key here 
-*/
+//no need to bother with passwords.h, this is a public feed
+#define PUBLICKEY        "keoG2q2KDQf3mpLxXEdWS1evp3j" // replace your phant public key here
 
 // SERVER INFO / DEFINITIONS
 #define SERVERADDRESS    "http://thingternet.club" //where does this phant instance live?
@@ -53,14 +48,13 @@ void setup() {
 
   //***************************************************************//
   // comment out this line if you want Yun to run w/o console open:
-  //while (!Console);   // wait for Network Serial to open
+  while (!Console);   // wait for Network Serial to open
   //***************************************************************//
 
   Console.println("booting up phant.io client");
 
   // Do a first update immediately
-  updateData();
-  sendData();
+  getData();
   lastRequest = millis();
 }
 
@@ -73,67 +67,24 @@ void loop() {
   if (currTime - lastRequest >= postingInterval) {
       if(serverResponded) Console.println("\n>>SERVER RESPONDED");
       else Console.println("\n>>LAST REQUEST FAILED");
-      updateData();
-      sendData();
+      getData();
       lastRequest = currTime;
   }
-  
-  delay(10); //just a little chill time between loops, not truly necessary
-  
-  /** here you can do lots of stuff like **/
-  // - update any i2c or digital sensors
-  // - show sensor data visualized/expressed locally
-  // - maybe check someone else datastream? make a getData() function and timer?
-  
 }
 
 
-//read any sensors, do any math
-void updateData() {
-  
-  // poll an analog pin, store in variable
-  analogSensorVal = analogRead(0); 
-  
-  //** generate some fake data waveforms **//
-  // sine wave:
-  //http://en.wikipedia.org/wiki/Sine_wave
-  sineVal += sineMod;
-  if(sineVal > 255 || sineVal <= 0) sineMod *= -1;
-
-  //other waveforms: http://en.wikipedia.org/wiki/Non-sinusoidal_waveform
-  //ramp/saw wave:
-  rampVal += abs(sineMod);
-  if(rampVal > 260) rampVal = 0;
-
-  //square wave:
-  if(squareVal > 0) squareVal = 0;
-  else squareVal = 255;
-
-
-  //** update values to the dataString to be sent **//
-  dataString = "sine=";       // must match name as setup on phant server
-  dataString += sineVal;
-  dataString += "&square=";   // must match name as setup on phant server
-  dataString += squareVal;
-  dataString += "&ramp=";     // must match name as setup on phant server
-  dataString += rampVal;
-  dataString += "&sensor1=";  // must match name as setup on phant server
-  dataString += analogSensorVal;
-}
 
 // this method makes a HTTP request to the server:
-void sendData() {
+void getData() {
   // form the string for the API header parameter:
   // form the string for the URL parameter:
 //  String url = "http://data.sparkfun.com/input/"; //BOOOOO
   String url = SERVERADDRESS;
-  url +="/input/";   //because we're inputting data into the server
+  url +="/output/";   //because we're outputting data FROM the server
   url += PUBLICKEY;
-  url += "?private_key=";
-  url += PRIVATEKEY;
-  url += "&";
-  url += dataString; //finally add in our dataString
-
+  url += ".csv"; //".json" //".jsonp"
+  url += "?page=1"; //just the lastest 50kb worth of data
+  
   // Send the server an HTTP GET request
   HttpClient client;
   Console.print("\n>>> Attempting Server Request \nclient.get:  ");
